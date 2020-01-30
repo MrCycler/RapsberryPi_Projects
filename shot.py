9030993fab3d242c25f4429ef98227c751c79ad1
Juan Mendoza <mendoza.jm@pucp.pe>
2020-01-30 12:29:26 -0500

    training images

--- shot.py
+++ shot.py
@@ -4,15 +4,19 @@ s, img = cam.read()
 
 winName = "Movement Indicator"
 cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
+cuenta = 0
 
 while s:
   cv2.imshow( winName,img )
-
   s, img = cam.read()
-
   key = cv2.waitKey(10)
-  if key == 27:
+  if key == ord('s'):
+    cv2.imwrite("img"+str(cuenta)+".jpg", img)
+    print("imagen guardada Numero")
+    print(cuenta)
+    cuenta = cuenta + 1
+  if key == ord('q'):
     cv2.destroyWindow(winName)
     break
 
-print ("Goodbye")
+print ("Finalizado")
